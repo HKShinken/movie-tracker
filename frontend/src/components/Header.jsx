403f4cfe8ea34fb81f51b384edb70ac718c6c826
HKShinken <108626067+HKShinken@users.noreply.github.com>
2025-09-03 16:22:28 +0200

    added navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,14 +5,16 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-primary-subtle border-bottom border-primary-subtle" variant="light">
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
+            
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
+
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -24,6 +26,7 @@ function Header() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
+
           </Nav>
         </Navbar.Collapse>
       </Container>
